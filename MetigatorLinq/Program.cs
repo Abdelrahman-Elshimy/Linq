776ad0894f47580395c82a757c27a662d9564d0a
Abdelrahman .Net <abdoelsimy123@gmail.com>
2022-01-02 03:15:31 +0200

    Add project files.

---
+++ MetigatorLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetigatorLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Linq");
+        }
+    }
+}
---
+++ PureVSImpureFunction/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PureVSImpureFunction
+{
+    // Pure Function 
+    /*
+     * 1 - Easy to test
+     * 2 - doesn't change in global variables so it no have a side effect
+     * 
+     */
+    class Program
+    {
+        static List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        static void Main(string[] args)
+        {
+            //AddInteger1(23);
+            
+
+            var x = 232;
+            AddInteger2(ref x);
+            Print(numbers);
+            Console.WriteLine(x);
+        }
+
+        static void Print(List<int> source)
+        {
+            foreach(int num in source)
+            {
+                Console.Write($" {num}");
+            }
+            Console.WriteLine();
+        }
+        // Impure Function
+        static void AddInteger1(int num)
+        {
+            numbers.Add(num);
+        }
+
+        static void AddInteger2(ref int num)
+        {
+            num++;
+            numbers.Add(num);
+        }
+    }
+}
