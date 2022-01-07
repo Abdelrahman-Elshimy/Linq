729a09fb888b8a3fa91ae85613649cb0b36ff4fa
Abdelrahman .Net <abdoelsimy123@gmail.com>
2022-01-07 16:56:31 +0200

    Delegate

---
+++ CADelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CADelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M2(M1);
+        }
+
+        static void M1()
+        {
+            Console.WriteLine("M 1");
+        }
+        static void M2(Action md)
+        {
+            md();
+            Console.WriteLine("M 2");
+        }
+    }
+}
---
+++ Function-ProgramingLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Function_ProgramingLinq
+{
+    class Program
+    {
+        // Function Programming
+        // Programming paradigm for developing software using functions
+        // Function Programming is the key part of function programming
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
